a241ec68cd1634bb2af9dfd9b3d63d5a758d081b
Carlo Echon <carloechon1@gmail.com>
2024-07-29 13:20:21 +0800

    Initial Commit
    Basic Functionality for a Initiative Tracker for games like D&D 5e or Pathfinder 1e or 2e.

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple_initiative_tracker_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/simple_initiative_tracker_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_initiative_tracker_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
